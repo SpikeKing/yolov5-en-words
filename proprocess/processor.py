42572e2c8ef92010b8c8c64f1f38f0dd566562f7
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2021-07-16 15:49:02 +0800

    update

--- proprocess/processor.py
+++ proprocess/processor.py
@@ -19,6 +19,7 @@ from root_dir import DATA_DIR
 from myutils.project_utils import *
 from myutils.cv_utils import *
 
+
 class Processor(object):
     def __init__(self):
         self.data_path = os.path.join(DATA_DIR, 'en_full.json')
@@ -51,9 +52,9 @@ class Processor(object):
         for jdx, clz in enumerate(clz_dict.keys()):
             box_list = clz_dict[clz]
             if box_list:
-                box_list, _ = filer_boxes_by_size(box_list)
-                # print('[Info] box_list: {}'.format(box_list))
-                img_bgr = draw_box_list(img_bgr, box_list, color=color_list[int(clz)], is_new=False)
+                # box_list, _ = filer_boxes_by_size(box_list)
+                # img_bgr = draw_box_list(img_bgr, box_list, color=color_list[int(clz)], is_new=False)
+                img_bgr = draw_rec_list(img_bgr, box_list, color=color_list[int(clz)], is_new=False)
                 cv2.imwrite(out_path, img_bgr)
 
     @staticmethod
@@ -165,10 +166,97 @@ class Processor(object):
         print("[Info] 处理完成: {}".format(out_dir))
 
 
+    @staticmethod
+    def save_img_patch(img_bgr, img_name):
+        """
+        上传图像
+        """
+        from x_utils.oss_utils import save_img_2_oss
+        oss_root_dir = "zhengsheng.wcl/Character-Detection/datasets/english-words-imgs-20210716/"
+        img_url = save_img_2_oss(img_bgr, img_name, oss_root_dir)
+        return img_url
+
+    @staticmethod
+    def format_word_dict(clz_dict, hw_rec_box):
+        hw_bbox = rec2bbox(hw_rec_box)
+        x_min, y_min = hw_bbox[0], hw_bbox[1]
+        res_dict = collections.defaultdict(list)
+        type_dict = {"0": "YingYuDanCi", "1": "ZhongWenShouXie", "2": "TuGai"}
+        item_list = []
+        for clz in clz_dict.keys():
+            box_list = clz_dict[clz]
+            for box in box_list:
+                box = [box[0] + x_min, box[1] + y_min, box[2] + x_min, box[3] + y_min]
+                rec = bbox2rec(box)
+                type_name = type_dict[clz]
+                item = {"coords": rec, "type": type_name}
+                item_list.append(item)
+        return item_list
+
+    @staticmethod
+    def process_img_line(image_data_list, image_url, out_path, img_idx):
+        angle, _, _ = image_data_list[0]
+        _, img_bgr = download_url_img(image_url)
+        img_bgr = rotate_img_for_4angle(img_bgr, angle)  # 旋转图像
+        height, width, _ = img_bgr.shape
+
+        image_name_x = image_url.split("/")[-1].split(".")[0]
+        image_name = "{}_{}.jpg".format(image_name_x, angle)
+        image_url = Processor.save_img_patch(img_bgr, image_name)
+
+        res_items = []
+        for image_data in image_data_list:
+            _, pos_data, clz_dict = image_data
+            hw_rec_box = Processor.parse_pos_2_rec(pos_data)
+            # print('[Info] rec_box: {}'.format(hw_rec_box))
+            item = {"coords": hw_rec_box, "type": "WenBenHang"}
+            item_list = Processor.format_word_dict(clz_dict, hw_rec_box)
+            # print('[Info] clz_dict: {}'.format(clz_dict))
+            res_items.append(item)
+            res_items += item_list
+
+        res_dict = {
+            "image_url": image_url,
+            "height": str(height),
+            "width": str(width),
+            "polygon_annotation": res_items
+        }
+
+        res_dict_str = json.dumps(res_dict)
+        write_line(out_path, res_dict_str)
+        print('[Info] {} 处理完成: {}'.format(img_idx, image_url))
+
+    def format_data(self):
+        img_path = os.path.join(DATA_DIR, 'en_full_with_alter.out-filter.txt')
+        out_path = os.path.join(DATA_DIR, 'en_full_with_alter.anno-{}.txt'.format(get_current_time_str()))
+        data_lines = read_file(img_path)
+        print('[Info] 文本行数: {}'.format(len(data_lines)))
+        image_dict = collections.defaultdict(list)
+        for data_line in data_lines:
+            data_dict = json.loads(data_line)
+            image_url = data_dict["image_url"]
+            angle = data_dict["angle"]
+            pos_data = data_dict["ocr_result"]
+            clz_dict = data_dict["clz_dict"]
+            image_dict[image_url].append([angle, pos_data, clz_dict])
+        print('[Info] 图像数: {}'.format(len(image_dict.keys())))
+
+        pool = Pool(10)
+        for img_idx, image_url in enumerate(image_dict.keys()):
+            image_data_list = image_dict[image_url]
+            # Processor.process_img_line(image_data_list, image_url, out_path, img_idx)
+            pool.apply_async(Processor.process_img_line, (image_data_list, image_url, out_path, img_idx))
+
+        pool.close()
+        pool.join()
+        print('[Info] 全部完成: {}'.format(out_path))
+
+
 def main():
     pro = Processor()
     # pro.process_mul()
-    pro.data_checker()
+    # pro.data_checker()
+    pro.format_data()
 
 
 if __name__ == '__main__':
